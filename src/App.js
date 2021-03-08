7c9f432537b0b11a44631f6c3d2c867b3a908bf3
mmarsden89 <mattmarsdendev@gmail.com>
2021-03-08 11:47:36 -0500

    add navbar to component heirarchy

--- src/App.js
+++ src/App.js
@@ -1,45 +1,7 @@
 import "./App.scss";
-import TopBar from "harmonium/lib/TopBar";
-import Menu from "harmonium/lib/Menu";
-import Form from "harmonium/lib/Form";
-import InputGroup from "harmonium/lib/InputGroup";
-import Button from "harmonium/lib/Button";
-import Brand from "harmonium/lib/Brand";
-import Input from "harmonium/lib/Input";
 
 function App() {
-  return (
-    <div className="App">
-      <TopBar>
-        <Brand
-          linkPath="/"
-          imagePath={process.env.PUBLIC_URL + "/images/logo.png"}
-          altTag="logo alt tag"
-          children="Hello Menu"
-        />
-        <TopBar.Item>
-          <Menu horizontalLeft>
-            <Menu.Item>
-              <a href="#one">One</a>
-            </Menu.Item>
-            <Menu.Item>
-              <a href="#two">Two</a>
-            </Menu.Item>
-          </Menu>
-        </TopBar.Item>
-        <TopBar.Item>
-          <Form>
-            <InputGroup>
-              <InputGroup.Field>
-                <Input type="text" placeholder="Search" />
-              </InputGroup.Field>
-              <Button>Search</Button>
-            </InputGroup>
-          </Form>
-        </TopBar.Item>
-      </TopBar>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,38 @@
+import TopBar from "harmonium/lib/TopBar";
+import Menu from "harmonium/lib/Menu";
+import Form from "harmonium/lib/Form";
+import InputGroup from "harmonium/lib/InputGroup";
+import Button from "harmonium/lib/Button";
+import Brand from "harmonium/lib/Brand";
+import Input from "harmonium/lib/Input";
+
+const Navbar = () => {
+  <TopBar>
+    <Brand
+      linkPath="/"
+      imagePath={process.env.PUBLIC_URL + "/images/logo.png"}
+      altTag="logo alt tag"
+      children="Hello Menu"
+    />
+    <TopBar.Item>
+      <Menu horizontalLeft>
+        <Menu.Item>
+          <a href="#one">One</a>
+        </Menu.Item>
+        <Menu.Item>
+          <a href="#two">Two</a>
+        </Menu.Item>
+      </Menu>
+    </TopBar.Item>
+    <TopBar.Item>
+      <Form>
+        <InputGroup>
+          <InputGroup.Field>
+            <Input type="text" placeholder="Search" />
+          </InputGroup.Field>
+          <Button>Search</Button>
+        </InputGroup>
+      </Form>
+    </TopBar.Item>
+  </TopBar>;
+};
