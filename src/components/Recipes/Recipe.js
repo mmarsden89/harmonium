c96ef58e8c572a45ee7eaab17b473f869c630d70
mmarsden89 <mattmarsdendev@gmail.com>
2021-03-10 12:55:59 -0500

    work on card component for recipe api return

--- src/components/Recipes/Recipe.js
+++ src/components/Recipes/Recipe.js
@@ -1,5 +1,8 @@
 import fetchRecipes from "./api";
 import React, { useEffect, useState } from "react";
+import Card from "harmonium/lib/Card";
+import Col from "harmonium/lib/Col";
+import Row from "harmonium/lib/Row";
 
 const Recipe = (props) => {
   const [recipes, setRecipes] = useState([]);
@@ -7,9 +10,29 @@ const Recipe = (props) => {
   useEffect(async () => {
     const recipesFetch = await fetchRecipes();
     setRecipes(recipesFetch);
+    console.log(recipesFetch);
   }, []);
 
-  const recipeMap = recipes.map((item) => <div>hi</div>);
+  const recipeMap = recipes.map((item) => (
+    <Col large={3}>
+      <Card primary>
+        <Card.Header>
+          <Row className="rev-Row--flex rev-Row--middle">
+            <Col>
+              <h5>{item.title}</h5>
+            </Col>
+          </Row>
+        </Card.Header>
+        <Card.Body>
+          <Row>
+            <Col>
+              <img src={item.image} className="ResponsiveImage" />
+            </Col>
+          </Row>
+        </Card.Body>
+      </Card>
+    </Col>
+  ));
 
   return <div>{recipeMap}</div>;
 };
