3d53eebe131a7744d572d941039f30f2c3b5a8d2
mmarsden89 <mattmarsdendev@gmail.com>
2021-03-23 16:38:27 -0400

    add browser router paths into app layer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import "./App.scss";
 import { Navbar, Recipe } from "./components/index";
+import { Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <Recipe />
+      <Switch>
+        <Route path="/" component={Recipe} />
+      </Switch>
     </div>
   );
 }
